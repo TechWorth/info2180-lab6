e505b09f45c54179d7aafc0f1afb2d946d503285
TechWorth <lornemiller21@gmail.com>
2019-11-12 00:14:28 -0500

    additional code

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-var btn = document.getElementById("search");
-var http = new XMLHttpRequest();
-var lst = "";
+window.onload = function() {
+    var btn = document.getElementById("search-btn");
+    var result = document.getElementById("result");
+    var input = '';
+    var lst = "";
+    $.get("superheroes.php", function(data) {
+        console.log(data);
+        lst = data;
 
-http.onreadystatechange = function() {
-    if (http.readyState == 4 && http.status == 200) {
-        lst = http.response;
+    });
+
+    function getValue() {
+        input = document.getElementById('search-bar');
+        var result = document.getElementById('result').value;
 
     }
+    //lst.getElementsByTagName('li');
+    btn.onclick = function() {
+        input.innerHTML = result;
+
+    }
+
+
 };
-
-http.open("GET", "superheroes.php", true);
-http.send();
-
-
-btn.onclick = function() {
-    alert(lst);
-}
